285e6ce0d28daba3fb1c5a38cc2a6c088b651894
Grace Ko <grace.ko@cybereason.com>
2022-01-25 01:29:50 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    console.log("ready")
+    $('.confetti').confettiButton({
+        colorArray: ['rgba(0, 0, 0, 0.3)', 'rgba(255, 255, 255, 0.3)'],
+        speed: 6,
+        hoverOnly: false
+    });
+  
+});
